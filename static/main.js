64d9131900b672b5e7a9d53c4411864cd6b6a77d
jasonkim094 <67155876+jasonkim094@users.noreply.github.com>
2020-08-31 01:41:21 +0900

    displayList Over

--- static/main.js
+++ static/main.js
@@ -1,8 +1,3 @@
-// 전역변수!!
-var dates = [];
-var rates = [];
-var keywords = [];
-var contents = [];
 // 이미지 로드되기 전에
 $(document).ready(function() {
     const historyElement = document.getElementById('history');
@@ -10,25 +5,6 @@ $(document).ready(function() {
     const rows = 5;
     let currentPage = 1;
 
-    $.ajax({
-        type: "GET",
-        url: "/diaries",
-        data: {},
-        success: function (response) {
-            let journals = response['history'];
-
-            for (let i = 0; i < journals.length; i++) {
-                let journal = journals[i];
-
-                dates.push(journal['date']);
-                rates.push(journal['rate']);
-                keywords.push(journal['keyword']);
-                contents.push(journal['content']);
-            }
-            // dates, rates, keywords, contents 라는 array 에 diary 정보가 포함되어 있음.
-            // console.log(dates)
-        }
-    });
     displayList(historyElement, rows, currentPage);
     setupPagination(paginationElement, rows);
 });
@@ -43,20 +19,16 @@ function displayList (wrapper, rowsPerPage, page) {
             if (response['result'] === 'success') {
                 wrapper.innerHTML = "";
                 page--;
-
+                let journals = response['history']
                 let start = page * rowsPerPage;
                 let end = start + rowsPerPage;
 
-                let paginatedDates = dates.slice(start, end);
-                let paginatedRates = rates.slice(start, end);
-                let paginatedKeywords = keywords.slice(start, end);
-                let paginatedContents = contents.slice(start, end);
-
-                for (let i = 0; i < paginatedDates.length; i++) {
-                    let paginatedDate = paginatedDates[i];
-                    let paginatedRate = paginatedRates[i];
-                    let paginatedKeyword = paginatedKeywords[i];
-                    let paginatedContent = paginatedContents[i];
+                for (let i = 0; i < journals.length; i++) {
+                    let journal = journals[i];
+                    let date = journal['date'];
+                    let rate = journal['rate'];
+                    let keyword = journal['keyword'];
+                    let content = journal['content'];
 
                     let journalElement = document.createElement('tr');
                     journalElement.classList.add('item');
@@ -66,10 +38,10 @@ function displayList (wrapper, rowsPerPage, page) {
                     let journalElementItem3 = document.createElement('td');
                     let journalElementItem4 = document.createElement('td');
 
-                    journalElementItem1.innerText = paginatedDate;
-                    journalElementItem2.innerText = paginatedRate;
-                    journalElementItem3.innerText = paginatedKeyword;
-                    journalElementItem4.innerText = paginatedContent;
+                    journalElementItem1.innerText = date;
+                    journalElementItem2.innerText = rate;
+                    journalElementItem3.innerText = keyword;
+                    journalElementItem4.innerText = content;
 
                     journalElement.appendChild(journalElementItem1);
                     journalElement.appendChild(journalElementItem2);
@@ -77,13 +49,12 @@ function displayList (wrapper, rowsPerPage, page) {
                     journalElement.appendChild(journalElementItem4);
 
                     wrapper.appendChild(journalElement);
-                }
-            }
+                };
+            };
         }
-    })
-}
+    });
+};
 
 function setupPagination(wrapper, rowsPerPage) {
     wrapper.innerHTML = "";
-    let pageCount = Math.ceil(dates.length / rowsPerPage);
 }
